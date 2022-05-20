937487ff928da3dc98588e3fe2a32dac44b63678
moishi-rand <randmeb@gmail.com>
2022-05-20 18:14:16 +0300

    added service to display score with flask

--- Controller/main_scores.py
+++ Controller/main_scores.py
@@ -1,3 +1,4 @@
+from flask import Flask
 
 # Return string as html format for display score
 def score_server():
@@ -5,15 +6,15 @@ def score_server():
     str_for_html = ""
 
     try:
-        score_file = score_file = open("my_text.txt", "r")
+        score_file = score_file = open("../Utills/my_text.txt", "r")
         player_score = score_file.read()
-        str_for_html = f""""
+        str_for_html = f"""
             <html>
                 <head>
                     <title>Scores Game</title>
                 </head>
                 <body>
-                    <h1>The score is <div id="score">{player_score}</div></h1>
+                    <h1>The score is: <div id="score">{player_score}</div></h1>
                 </body>
             </html>
         """
@@ -32,4 +33,14 @@ def score_server():
                 """
 
     return str_for_html
+
+#Run service of display main score
+app = Flask(__name__)
+
+# accessed via <HOST>:<PORT>/
+@app.route("/")
+def welcome():
+    return score_server(), 200 # status code
+
+app.run(host='127.0.0.1', debug=True, port=5000)
 
